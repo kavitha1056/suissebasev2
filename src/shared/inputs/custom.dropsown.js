b869c65a83e6c850005dc23271a4cd78b179628f
subbareddy <subbareddy@zirafftechnologies.com>
2022-01-13 14:51:02 +0530

    space

--- src/shared/inputs/custom.dropsown.js
+++ src/shared/inputs/custom.dropsown.js
@@ -4,8 +4,8 @@ import { Component } from "react";
 class CustomDropdown extends Component {
 
     render() {
-        const { data = [], valueField, textField ,placeholder} = this.props
-        return <Select showSearch {...this.props} placeholder={placeholder||"Please select an option"}>
+        const { data = [], valueField, textField, placeholder } = this.props
+        return <Select showSearch {...this.props} placeholder={placeholder || "Please select an option"}>
             {data?.map(item => <Select.Option value={item[valueField]}>{item[textField]}</Select.Option>)}
         </Select>
     }
