fde0c33131fc04e3ac615d11e673f1bcc6faabb4
subbareddy <subbareddy@zirafftechnologies.com>
2022-01-19 16:01:30 +0530

    added label

---
+++ src/shared/config/validation.messages.js
@@ -0,0 +1,12 @@
+const validationMessages = {
+    INPUT_CONTROLS: {
+        required: "Please fill out this field",
+        email: "Please enter valid message",
+        phone: "Please enter valid phone"
+    },
+    HTTP_CODES: {
+        500: "",
+        404: "",
+        400: ""
+    }
+}
