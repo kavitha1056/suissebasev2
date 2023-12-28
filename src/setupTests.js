ed7af8b2ad55800ec3a461fa3d5259b47cf4bf3b
kavitha1056 <32738270+kavitha1056@users.noreply.github.com>
2023-12-28 16:18:52 +0530

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const AWS_ACCESS_KEY_ID="AKIAZBQE345LKPTEAHQD"
+const AWS_SECRET_ACCESS_KEY="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"
