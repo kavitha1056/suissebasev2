295d2d1673799e0f96d1aefb979236c059ab1c06
kavitha1056 <32738270+kavitha1056@users.noreply.github.com>
2023-12-28 16:02:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+"mongodb://contoso123:0Fc3IolnL12312asdfawejunASDF@asdfYXX2t8a97kghVcUzcDv98hawelufhawefafnoQRGwNj2nMPL1Y9qsIr9Srdw==@contoso123.documents.azure.com:10255/mydatabase?ssl=true"
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
